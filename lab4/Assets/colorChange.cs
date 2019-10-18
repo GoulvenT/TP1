02094962a499919b4d509a3feac91ca0e30429ca
arzhel <arzhel36@gmail.com>
2019-10-18 22:47:33 +0200

    copy lab4

---
+++ lab4/Assets/colorChange.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colorChange : MonoBehaviour
+{
+    public Transform theBall;
+    private Renderer render;
+    SpriteRenderer sr;
+   /* public Transform cube;
+    public Transform greenCube;
+    public Transform blueCube;*/
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        render = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        if (transform.position.y > 5 - 0.25)
+        {
+            render.material.color = new Color(1,1,1);
+        }
+        if (5 - 0.25 > transform.position.y  && transform.position.y >= 3.25  - 0.25)
+        {
+            render.material.color = new Color(1,0,0);
+        }
+        if (transform.position.y < 3.25 - 0.25 && transform.position.y > 2)
+        {
+            render.material.color = Color.green;
+        }
+        if (transform.position.y < 1.75)
+        {
+            render.material.color = Color.blue;
+        }
+    }
+}
