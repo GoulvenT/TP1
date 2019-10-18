744e0c7e7a276ace78824086a6c4cad36cfc4416
arzhel <arzhel36@gmail.com>
2019-10-19 01:35:50 +0200

    jeu avec physique d'unity

---
+++ lab4/Assets/gameLoop.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+public class gameLoop : MonoBehaviour 
+{
+    // Reference to the Prefab. Drag a Prefab into this field in the Inspector.
+    public GameObject myPrefab;
+    public Transform boule;
+    public Boolean myturn;
+    private GameObject clone;
+
+    private Boolean haveToCreate = true;
+    // This script will simply instantiate the Prefab when the game starts.
+    void Start()
+    {
+        //Instantiate(myPrefab, new Vector3(-1.5f, 17, -1.5f), Quaternion.identity);
+    }
+
+    private void Update()
+    {
+        if (myturn)
+        {
+            Debug.Log(sphere.echec);
+            if (haveToCreate)
+            {
+                clone = Instantiate(myPrefab, new Vector3(-1.5f, 17, -1.5f), Quaternion.identity);
+                haveToCreate = false;
+            }
+
+            if (Input.GetKey(KeyCode.LeftArrow) && clone.transform.position.x < 4.5f)
+            {
+                clone.transform.Translate(0.1f, 0, 0);
+            }
+
+            if (Input.GetKey(KeyCode.RightArrow) && clone.transform.position.x > -4.5f)
+            {
+                clone.transform.Translate(-0.1f, 0, 0);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                sphere.setWin(false);
+                sphere.setGravity(true);
+
+                myturn = false;
+            }
+        }
+
+        if (sphere.echec)
+        {
+            myturn = true;
+            sphere.setGravity(false);
+            Destroy(clone, 0.1f);
+            haveToCreate = true;
+            sphere.setEchec(false);
+        }
+
+        if (sphere.win)
+        {
+            sphere.setEchec(false);
+            myturn = false;
+        }
+    }
+}
---
+++ lab4/Assets/sphere.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sphere : MonoBehaviour
+{
+    public Transform fail;
+    public Transform arrive;
+    public Rigidbody  boule;
+    public static Boolean hasGravity = false;
+    public static Boolean echec = false;
+    public static Boolean win = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        boule.useGravity = false;
+    }
+
+    public static void  setGravity(Boolean b)
+    {
+        hasGravity = b;
+    }
+    public static void  setWin(Boolean b)
+    {
+        win = b;
+    }
+    public static void  setEchec(Boolean b)
+    {
+        echec = b;
+    }
+    // Update is called once per frame
+    private void OnTriggerStay(Collider collide)
+    {
+        if (collide == arrive.GetComponent<Collider>())
+        {
+            hasGravity = false;
+            boule.velocity = Vector3.zero;
+            win = true;
+        }
+
+        if (collide == fail.GetComponent<Collider>())
+        {
+
+            echec = true ;
+        }
+        
+    }
+
+    void Update()
+    {
+        if (hasGravity)
+        {
+            boule.useGravity = true;
+        }
+    }
+}
